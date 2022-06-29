8c54fb99c26e51ac5f8eeb81ffc78b65265c04aa
juneglee <klcpop1@gmail.com>
2022-06-29 23:41:00 +0900

    [add] cout, cin

---
+++ exam/chapter01/ex02.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int ex02() {
+	int x = 0;
+	std::cout << "Input the number : ";
+	std::cin >> x;
+	std::cout << "x = " << x << std::endl;
+	return 0;
+}
---
+++ exam/chapter01/ex03.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int ex03(void) {
+	bool b = true;
+	if (b)
+		std::cout << "b is true" << std::endl;
+	else
+		std::cout << "b is false" << std::endl;
+	return 0;
+}
