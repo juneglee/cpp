9fde4036e2649928f8d6ae74bdab06cb0caf286e
juneglee <klcpop1@gmail.com>
2022-06-30 23:52:10 +0900

    [Add] Factorial

---
+++ exam/exam01/ex05.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//  ÇÔ¼ö
+// Factorial 
+
+int Factorial(int _iNum)
+{
+	// int i = 2;
+	int iValue = 1;
+
+	for (int j = 0; j < _iNum - 1; ++j)
+	{
+		iValue *= (j + 2);
+	}
+
+	return iValue;
+}
+
+int main()
+{
+	int iValue = Factorial(4);
+
+	iValue = Factorial(10);
+
+	return 0;
+
+}
