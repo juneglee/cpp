e4a43527606fc07525c4bd6e094befe533b5b490
juneglee <klcpop1@gmail.com>
2022-07-02 23:25:17 +0900

    init

--- c++/Project1/ex2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "소프트웨어\n" << std::endl;
-
-	return 0;
-
-}
--- exam/exam01/ex02.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-	int x = 0;
-	std::cout << "Input the number : ";
-	std::cin >> x;
-	std::cout << "x = " << x << std::endl;
-	return 0;
-}
