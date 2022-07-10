a6ef91d5be12bc1514051f528b08bcdd1d4d53c1
juneglee <klcpop1@gmail.com>
2022-07-10 23:37:08 +0900

    [add] exam

--- exam/ex01.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int ex01(void) {
-	int x = 10, y = 20;
-	std::cout << "x = " << x << ", y = " << y << std::endl;
-	return 0;
-}
--- exam/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "func.h"
-
-int main()
-{
-	int i = Mul(10, 20);
-	int a = Add(10, 20);
-
-	return 0;
-}
