31b98eb3edab313fac61a3b69e718c4889be8125
juneglee <klcpop1@gmail.com>
2022-07-10 23:36:13 +0900

    [add] exam

---
+++ exam/ex/func.cpp
@@ -0,0 +1,18 @@
+#include "func.h"
+
+int g_i = 0;
+
+int Add(int a, int b) 
+{
+	return a + b;
+}
+
+int Sub(int a, int b) 
+{
+	return a - b;
+}
+
+int Mul(int a, int b)
+{
+	return a * b;
+}
---
+++ exam/ex/main.cpp
@@ -0,0 +1,9 @@
+#include "func.h"
+
+int main()
+{
+	int i = Mul(10, 20);
+	int a = Add(10, 20);
+
+	return 0;
+}
